d239a35f3514e32afcf12318edb89265c93711b3
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-10-21 15:23:40 +0700

    fix: fix conflict

--- src/components/seller/Product/ProductViewDialog.jsx
+++ src/components/seller/Product/ProductViewDialog.jsx
@@ -5,8 +5,7 @@ import ActionButton from "../../buttonCustom/ActionButton.jsx";
 import {DASHBOARD_STYLES} from '../../constants.js';
 
 const ProductViewDialog = ({
-                               open,
-                               onClose,
+                               open,onClose,
                                selectedProduct,
                                getStatusLabel,
                                getStatusColor,
