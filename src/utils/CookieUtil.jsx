b5e9fdbc3b7537b23356588bef46e0be7a138cbf
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-10-29 01:21:51 +0700

    fix: remove log out

--- src/utils/CookieUtil.jsx
+++ src/utils/CookieUtil.jsx
@@ -1,10 +1,14 @@
 import {getAccess} from "../services/AccountService.jsx";
 
 export const getAccessToken = async () => {
+    console.log("I am here 3")
     const response = await getAccess();
 
+    console.log("I am here 4")
     if (response && response.status === 200) {
+        console.log("I am here 5")
         return response.data.data.access
     }
+    console.log("I am here 6")
     return null
 };
