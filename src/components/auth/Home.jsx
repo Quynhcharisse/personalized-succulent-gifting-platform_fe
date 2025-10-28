43984f8502e3bdab41936108c6c0b882423494be
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-10-28 22:41:46 +0700

    fix: fix UI

--- src/components/auth/Home.jsx
+++ src/components/auth/Home.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import {Button} from '@mui/material'
 import '../../styles/auth/Home.css'
 import ChatBot from './ChatBot.jsx'
